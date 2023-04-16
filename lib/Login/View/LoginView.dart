982f480705665dba9aa2c81c03cc4bb3192c2805
sub101 <subin10@nate.com>
2023-04-16 19:05:31 +0900

    로그인뷰 수정

--- lib/Login/View/LoginView.dart
+++ lib/Login/View/LoginView.dart
@@ -21,110 +21,125 @@ class _LoginViewState extends State<LoginView> {
       builder: (context, authService, child) {
         final user = authService.currentUser();
         return Scaffold(
-          appBar: AppBar(title: Text("로그인")),
-          body: SingleChildScrollView(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Center(
-                  child: Text(
-                    "WeatherFit",
-                    style: TextStyle(
-                      fontSize: 24,
+          body: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                  Color.fromARGB(255, 95, 171, 233),
+                  Color.fromARGB(255, 197, 234, 255)
+                ])),
+            alignment: Alignment.center,
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Text(
+                      "WeatherFit",
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
                   ),
-                ),
-                SizedBox(height: 32),
+                  SizedBox(height: 32),
 
-                /// 이메일
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(hintText: "이메일"),
-                ),
+                  /// 이메일
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(hintText: "e-mail"),
+                  ),
 
-                /// 비밀번호
-                TextField(
-                  controller: passwordController,
-                  obscureText: true, // 비밀번호 안보이게
-                  decoration: InputDecoration(hintText: "비밀번호"),
-                ),
-                SizedBox(height: 32),
+                  /// 비밀번호
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true, // 비밀번호 안보이게
+                    decoration: InputDecoration(hintText: "password"),
+                  ),
+                  SizedBox(height: 32),
 
-                /// 로그인 버튼
-                ElevatedButton(
-                  child: Text("로그인", style: TextStyle(fontSize: 21)),
-                  onPressed: () {
-                    // 로그인
-                    authService.signIn(
-                      email: emailController.text,
-                      password: passwordController.text,
-                      onSuccess: () {
-                        // 로그인 성공
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text("로그인 성공"),
-                        ));
+                  /// 로그인 버튼
+                  ElevatedButton(
+                    child: Text("Login", style: TextStyle(fontSize: 21)),
+                    onPressed: () {
+                      // 로그인
+                      authService.signIn(
+                        email: emailController.text,
+                        password: passwordController.text,
+                        onSuccess: () {
+                          // 로그인 성공
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("로그인 성공"),
+                          ));
 
-                        // HomePage로 이동
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  BottomNavigationBarWidget()),
-                        );
-                      },
-                      onError: (err) {
-                        // 에러 발생
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text(err),
-                        ));
-                      },
-                    );
-                  },
-                ),
+                          // HomePage로 이동
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    BottomNavigationBarWidget()),
+                          );
+                        },
+                        onError: (err) {
+                          // 에러 발생
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(err),
+                          ));
+                        },
+                      );
+                    },
+                  ),
 
-                /// 회원가입 버튼
-                ElevatedButton(
-                  child: Text("회원가입", style: TextStyle(fontSize: 21)),
-                  onPressed: () {
-                    // 회원가입 화면으로 이동
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignupView()),
-                    );
-                  },
-                ),
+                  /// 회원가입 버튼
+                  ElevatedButton(
+                    child: Text("Sign up", style: TextStyle(fontSize: 21)),
+                    onPressed: () {
+                      // 회원가입 화면으로 이동
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignupView()),
+                      );
+                    },
+                  ),
 
-                /// 구글 계정으로 로그인 버튼
-                ElevatedButton(
-                  child: Text("구글 계정으로 로그인", style: TextStyle(fontSize: 21)),
-                  onPressed: () {
-                    // 구글 계정으로 로그인
-                    authService.signnInWithGoogle(
-                      onSuccess: () {
-                        // 로그인 성공
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text("로그인 성공"),
-                        ));
+                  /// 구글 계정으로 로그인 버튼
+                  ElevatedButton(
+                    child: Text("Sign In with Google",
+                        style: TextStyle(fontSize: 21)),
+                    onPressed: () {
+                      // 구글 계정으로 로그인
+                      authService.signnInWithGoogle(
+                        onSuccess: () {
+                          // 로그인 성공
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("로그인 성공"),
+                          ));
 
-                        // HomePage로 이동
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  BottomNavigationBarWidget()),
-                        );
-                      },
-                      onError: (err) {
-                        // 에러 발생
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text(err),
-                        ));
-                      },
-                    );
-                  },
-                ),
-              ],
+                          // HomePage로 이동
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    BottomNavigationBarWidget()),
+                          );
+                        },
+                        onError: (err) {
+                          // 에러 발생
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(err),
+                          ));
+                        },
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         );
