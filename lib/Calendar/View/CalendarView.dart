49d8941c1a309f0b9eb4e2d61c01839adb31e67f
sub101 <subin10@nate.com>
2023-04-25 10:14:09 +0900

    create/update 메소드 수정

--- lib/Calendar/View/CalendarView.dart
+++ lib/Calendar/View/CalendarView.dart
@@ -122,6 +122,24 @@ class _CalendarViewState extends State<CalendarView> {
     );
   }
 
+  void creatRecord(RecordService recordService) {
+    String newText = createTextController.text.trim();
+    if (newText.isNotEmpty) {
+      recordService.create(newText, selectedDate);
+      createTextController.text = "";
+    }
+  }
+
+  void updateRecord(RecordService recordService, Record record) {
+    String updatedText = updateTextController.text.trim();
+    if (updatedText.isNotEmpty) {
+      recordService.update(
+        record.createdAt,
+        updatedText,
+      );
+    }
+  }
+
   void showCreateDialog(RecordService recordService) {
     showDialog(
       context: context,
